4975188b4e3c2d4b7a54586c9eace29502b89e47
zbiggs <zbiggs@lesolsoncompany.com>
2023-06-22 09:46:38 -0600

    Updated ResumePage.tsx

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -6,7 +6,7 @@ export function ResumePage()
                 <h1 className="text-5xl text-white font-extrabold">Resume</h1>
             </header>
             <main className='main'>
-                <iframe style={{width: '750px', height: '800px'}} src="https://drive.google.com/file/d/1RouJv1yxpgizNaxlyMTW65_QZmAS0r9iwkAQubf8aF0/preview?usp=embed_googleplus" allowFullScreen></iframe>
+                <iframe style={{width: '750px', height: '800px'}} src="https://drive.google.com/file/d/1hgpf16amUmm_2lJKvHL8jodPLtIrYSdpqba1ZH3chJA/preview?usp=embed_googleplus" allowFullScreen></iframe>
             </main>
         </>
     );
